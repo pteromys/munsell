0a209f3f7e94393e392b00b98119d697aaf73d69
pteromys <pteromys@gmail.com>
2011-12-21 00:32:50 -0500

    Add interpolation and grays.
    Now we interpolate trilinearly between entries in the table.
    There is the start of an interface for exposing the interpolation.
    
    draw_selections is now its own function so that I can call it from
    draw_continuous as well.

---
+++ munsell-interpolate.js
@@ -0,0 +1,35 @@
+// Trilinearly interpolate.
+Munsell.interpolate = function (i, j, k) {
+	// modified multiplication to return 0 for 0 * NaN and 0 * Infinity.
+	function mul(factor, maybe_number) {
+		if (factor == 0) { return 0; }
+		else { return factor * maybe_number; }
+	}
+	// Insert gray
+	k = k + 1;
+	var i0 = Math.floor(i);
+	var j0 = Math.floor(j);
+	var k0 = Math.floor(k);
+	var i1 = (i0 + 1) % 40;
+	var j1 = Math.min(j0 + 1, 13);
+	var k1 = Math.min(k0 + 1, 24);
+	var a1 = i - i0;
+	var b1 = j - j0;
+	var c1 = k - k0;
+	var a0 = 1 - a1;
+	var b0 = 1 - b1;
+	var c0 = 1 - c1;
+	var ans = [0, 0, 0];
+	for (var t in ans) {
+		ans[t] =
+			mul(a0 * b0 * c0, Munsell[i0][j0][k0][t]) +
+			mul(a1 * b0 * c0, Munsell[i1][j0][k0][t]) +
+			mul(a0 * b1 * c0, Munsell[i0][j1][k0][t]) +
+			mul(a1 * b1 * c0, Munsell[i1][j1][k0][t]) +
+			mul(a0 * b0 * c1, Munsell[i0][j0][k1][t]) +
+			mul(a1 * b0 * c1, Munsell[i1][j0][k1][t]) +
+			mul(a0 * b1 * c1, Munsell[i0][j1][k1][t]) +
+			mul(a1 * b1 * c1, Munsell[i1][j1][k1][t]);
+	}
+	return ans;
+}
